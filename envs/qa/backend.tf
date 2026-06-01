2d81683f0272e219fd2b1c500598807880a7f3a2
Thulasi Krishna Gurramkonda <pasupuleti@Thulasis-MacBook-Pro.local>
2026-06-01 18:17:11 +0530

    config: update backend bucket name for my account

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-ranjiniganeshan"
+    bucket       = "zen-pharma-terraform-state-thulasi1983"
     key          = "envs/qa/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
