b4b00c4e63b09ab4b41a3a38585093731aef56f6
Thulasi Krishna Gurramkonda <pasupuleti@Thulasis-MacBook-Pro.local>
2026-06-03 18:54:52 +0530

    updated rds engine-version to 15.10

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "rds" {
 resource "aws_db_instance" "main" {
   identifier             = "${var.project}-${var.env}-postgres"
   engine                 = "postgres"
-  engine_version         = "15.7"
+  engine_version         = "15.10"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   storage_type           = "gp2"
